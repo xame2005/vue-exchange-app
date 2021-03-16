caaac679309f662a8615306ef55017c4fa341ff4
Samuel Gonzalez <xame2005@gmail.com>
2021-03-15 22:40:23 -0600

    [CREATED] Dynamic routes for CoinDetail component

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from "vue-router";
 import Home from "@/views/Home.vue";
 import About from "@/views/About.vue";
 import Error from "@/views/Error.vue";
+import CoinDetail from "@/views/CoinDetail";
 
 Vue.use(Router);
 
@@ -19,6 +20,11 @@ export default new Router({
       path: "/about",
       name: "about",
       component: About
+    },
+    {
+      path: "/coin/:id",
+      name: "coin-detail",
+      component: CoinDetail
     },
     {
       path: ":catchAll(.*)",
